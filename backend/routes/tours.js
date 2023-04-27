7e978d3336c2f0384d727883e1dd174e86d09123
Abhay <ggbois001@gmail.com>
2023-04-27 21:34:45 +0530

    second commit

--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -1,26 +1,26 @@
-
-import express from 'express';
-import { 
-    createTour,
-    deleteTour,
-    getAllTour, 
-    getFeaturedTour, 
-    getSingleTour,
-    getTourBySearch,
-    getTourCount,
-    updateTour,
-     } from '../controllers/tourController.js';
+import express from "express";
+import {
+  createTour,
+  deleteTour,
+  getAllTour,
+  getFeaturedTour,
+  getSingleTour,
+  getTourBySearch,
+  getTourCount,
+  updateTour,
+} from "../controllers/tourController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 // create New Tour
-router.post("/", createTour);
+router.post("/", verifyAdmin, createTour);
 
 // update Tour
-router.put("/:id", updateTour);
+router.put("/:id", verifyAdmin, updateTour);
 
 // delete Tour
-router.delete("/:id", deleteTour);
+router.delete("/:id", verifyAdmin, deleteTour);
 
 // getSingle Tour
 router.get("/:id", getSingleTour);
@@ -29,9 +29,9 @@ router.get("/:id", getSingleTour);
 router.get("/", getAllTour);
 
 //get tour by search
-router.get("/search/getTourBySearch",getTourBySearch);
-router.get("/search/getFeaturedTours",getFeaturedTour);
-router.get("/search/getTourCount",getTourCount);
+router.get("/search/getTourBySearch", getTourBySearch);
+router.get("/search/getFeaturedTours", getFeaturedTour);
+router.get("/search/getTourCount", getTourCount);
 
-export default router ; 
+export default router;
 
